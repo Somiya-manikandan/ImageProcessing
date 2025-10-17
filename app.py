99e18cd26407f66034d4031e7837f12fd9bea0e8
Somiya-manikandan <153815946+Somiya-manikandan@users.noreply.github.com>
2025-10-17 22:21:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 import streamlit as st
-
-# Title
+
 st.set_page_config(page_title="Image Color Filter App", layout="centered")
 st.title("🎨 Image Color Filter App")
-
-# Read your HTML file
+
 with open("index.html", "r", encoding="utf-8") as f:
     html_code = f.read()
-
-# Display it inside Streamlit
+
 st.components.v1.html(html_code, height=800, scrolling=True)
